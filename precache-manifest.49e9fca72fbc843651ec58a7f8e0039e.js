ff22b0fb29b95b5370ea36ddd508fdbef5353abe
ddarshankamath <ddarshankamath@gmail.com>
2022-09-19 15:47:34 +0530

    Updates

---
+++ precache-manifest.49e9fca72fbc843651ec58a7f8e0039e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7b7af9359764dd29e4ef3ef88d0d1a0",
+    "url": "/chat-app/index.html"
+  },
+  {
+    "revision": "e1fe91a5cfeee6920ea3",
+    "url": "/chat-app/static/css/main.aed7465d.chunk.css"
+  },
+  {
+    "revision": "c8e64a3344d2e92932ae",
+    "url": "/chat-app/static/js/2.64b70264.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/chat-app/static/js/2.64b70264.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1fe91a5cfeee6920ea3",
+    "url": "/chat-app/static/js/main.1c37e3ab.chunk.js"
+  },
+  {
+    "revision": "a46df673de3fe3887809",
+    "url": "/chat-app/static/js/runtime-main.0c4eda3f.js"
+  }
+]);
